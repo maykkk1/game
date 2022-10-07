f4c738faac09c7c322b939be0d5fd3381d61af86
Maycon Douglas <70534369+maykkk1@users.noreply.github.com>
2022-10-07 11:34:05 -0300

    adicionada mecanica de movimentacao

--- js/main.js
+++ js/main.js
@@ -1,9 +1,50 @@
-const sprite = document.getElementById('sprite')
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
 
-document.addEventListener('keypress', (evento) => {
-    if(evento.keyCode === 119) {
-        let position = sprite.offsetTop - 25
-        sprite.style.top = `${position}px`;
-        console.log(position)
+
+const sprite = new Image()
+sprite.src = './sprite.png'
+
+const personagem = {
+    sprite: sprite,
+    position_x: 600,
+    position_y: 280,
+    size: [40, 40],
+
+    draw() {
+        ctx.drawImage(
+            sprite, 
+            0, 0,
+            280, 274,
+            personagem.position_x, personagem.position_y,
+            personagem.size[0], personagem.size[1])
+    }
+}
+
+const fundo = {
+    draw() {
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+    }
+}
+
+const loop = () => {
+    fundo.draw()
+    personagem.draw()
+    requestAnimationFrame(loop)
+}
+
+loop();
+
+document.addEventListener('keypress', (event)=>{
+    if(event.keyCode === 119 && personagem.position_y != 0) {
+        personagem.position_y -= 40;
+    } else if (event.keyCode === 115 && personagem.position_y != canvas.height - 40) {
+        personagem.position_y += 40;
+    } else if (event.keyCode === 100 && personagem.position_x != canvas.width - 40) {
+        console.log(personagem.position_x)
+        personagem.position_x += 40;
+    } else if (event.keyCode === 97 && personagem.position_x != 0) {
+        personagem.position_x -= 40;
     }
 })
